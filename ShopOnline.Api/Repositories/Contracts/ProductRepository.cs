f86d7d29d3f82c47d387c42b8fbd0ded55f711b1
avadhoot ajit khadake <avadhoot196@gmail.com>
2024-03-10 14:24:50 +0530

    Add project files.

---
+++ ShopOnline.Api/Repositories/Contracts/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ShopOnline.Api.Repositories.Contracts
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext shopOnlineDbContext;
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            this.shopOnlineDbContext = shopOnlineDbContext;
+        }
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await this.shopOnlineDbContext.ProductCategories.ToListAsync();
+            return categories;
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var product= await this.shopOnlineDbContext.Products.ToListAsync();
+            return product;
+        }
+    }
+}
