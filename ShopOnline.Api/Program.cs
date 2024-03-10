0c2cb5bad70d19b8c2e4475b9357977422d8fc91
avadhoot ajit khadake <avadhoot196@gmail.com>
2024-03-10 17:52:45 +0530

    all changes

--- ShopOnline.Api/Program.cs
+++ ShopOnline.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 using ShopOnline.Api.Data;
 using ShopOnline.Api.Repositories.Contracts;
 
@@ -11,7 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContextPool<ShopOnlineDbContext>(options => 
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection")));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
@@ -22,6 +23,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(policy =>
+    policy.WithOrigins("http://localhost:7071/", "https://localhost:7071/")
+    .AllowAnyMethod()
+    .WithHeaders(HeaderNames.ContentType)
+    );
 
 app.UseHttpsRedirection();
 
